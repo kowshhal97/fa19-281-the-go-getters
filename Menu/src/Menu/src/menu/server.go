3385560e9ccdf822d438ef5fa7edf5cfbe42e4c2
Anjana Menon <anjanamenoncherubala@sjsu.edu>
2019-11-20 23:00:20 -0800

    Minor modifications in POST API

--- Menu/src/Menu/src/menu/server.go
+++ Menu/src/Menu/src/menu/server.go
@@ -2,38 +2,23 @@ package main
 
 import (
 	"fmt"
-//	"log"
-	"net/http"
 	"encoding/json"
 	"github.com/codegangsta/negroni"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
-	"github.com/gorilla/handlers"
 	"gopkg.in/mgo.v2"
-	"net"
-	"strings"
 	"gopkg.in/mgo.v2/bson"
-//	"os"
+	"net"
+	//	"log"
+	"net/http"
+	"strings"
+	//	"os"
 )
-
-// MongoDB Config
-// var database_server = "ds227185.mlab.com:27185"
-// var database = "counterburger"
-// var collection = "menu"
-// var mongo_user = "cmpe281"
-// var mongo_pass = "cmpe281"
-
-/*
-var database_server = os.Getenv("Server")
-var database = os.Getenv("Database")
-var collection = os.Getenv("Collection")
-var mongo_user = os.Getenv("User")
-var mongo_pass = os.Getenv("Pass")
-*/
 
 var database_server = "35.166.110.191:27017"
 var database = "admin"
-var collection = "users"
+var collection = "menu"
 var mongo_user = "admin"
 var mongo_pass = "admin"
 
@@ -56,30 +41,37 @@ func MenuServer() *negroni.Negroni {
 // Menu Service API Routes
 func initRoutes(router *mux.Router, formatter *render.Render) {
 	router.HandleFunc("/menu/ping", pingHandler(formatter)).Methods("GET")
-	router.HandleFunc("/menu/item", createItemHandler(formatter)).Methods("POST")
 	router.HandleFunc("/menu", GetMenu(formatter)).Methods("GET")
 	router.HandleFunc("/menu/{ItemId}", getItemByIdHandler(formatter)).Methods("GET")
+	router.HandleFunc("/menu/item", createItemHandler(formatter)).Methods("POST")
 }
 
+/* Error Helper Functions
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}*/
 
 // Menu Service Health Check API
 func pingHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-        message := "Pizza My Heart API Server is UP!: " + getSystemIp()
+		message := "Pizza My Heart API Server is UP!: " + getSystemIp()
 		formatter.JSON(w, http.StatusOK, struct{ Test string }{message})
 	}
 }
 
 func getSystemIp() string {
-    conn, err := net.Dial("udp", "8.8.8.8:80")
-    if err != nil {
-        return ""
-    }
-    defer conn.Close()
-    localAddr := conn.LocalAddr().(*net.UDPAddr).String()
-    address := strings.Split(localAddr, ":")
-    fmt.Println("address: ", address[0])
-    return address[0]
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return ""
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr).String()
+	address := strings.Split(localAddr, ":")
+	fmt.Println("address: ", address[0])
+	return address[0]
 }
 
 // API to find an item in the menu
@@ -87,33 +79,25 @@ func GetMenu(formatter *render.Render) http.HandlerFunc {
 	return func(response http.ResponseWriter, request *http.Request) {
 		fmt.Println("Here in Get")
 		session,err:= mgo.Dial(database_server)
-		if(err!=nil){
-		formatter.JSON(response, http.StatusInternalServerError, "Internal Server Error*****")
-        			return
-		}
+
 		//defer session.Close()
 		session.SetMode(mgo.Monotonic, true)
 		fmt.Println("user pass", mongo_user, mongo_pass)
-		err = session.DB(database_server).Login(mongo_user, mongo_pass)
-		if err != nil {
-			//log.Fatalf(" %s", err)
-			formatter.JSON(response, http.StatusInternalServerError, "Internal Server Error")
-			return
-		}
+		err= session.DB(database).Login(mongo_user, mongo_pass)
 		defer session.Close()
 		fmt.Println("Defer session close")
-        //session.SetMode(mgo.Monotonic, true) need to check-original comment
-        mongo_collection := session.DB(database).C(collection)
-        var result []Item
-        fmt.Println("Declaring err")
+		//session.SetMode(mgo.Monotonic, true) need to check-original comment
+		mongo_collection := session.DB(database).C(collection)
+		var result []Item
+		fmt.Println("Declaring err")
 		err = mongo_collection.Find(bson.M{}).All(&result)
 		fmt.Println("Result: ", result)
-        if err != nil {
+		if err != nil {
 			//log.Fatalf(" %s", err)
-            formatter.JSON(response, http.StatusNotFound, "Menu not found !!!")
-            return
-        }
-        fmt.Println("Menu : ", result)
+			formatter.JSON(response, http.StatusNotFound, "Menu not found !!!")
+			return
+		}
+		fmt.Println("Menu : ", result)
 		formatter.JSON(response, http.StatusOK, result)
 	}
 
@@ -126,8 +110,8 @@ func getItemByIdHandler(formatter *render.Render) http.HandlerFunc {
 		//Setup
 		session, error := mgo.Dial(database_server)
 		if error = session.DB(database).Login(mongo_user, mongo_pass); error != nil {
-		formatter.JSON(w, http.StatusInternalServerError, "Error")
-		return
+			formatter.JSON(w, http.StatusInternalServerError, "Error")
+			return
 		}
 		defer session.Close()
 		session.SetMode(mgo.Monotonic, true)
@@ -149,15 +133,15 @@ func getItemByIdHandler(formatter *render.Render) http.HandlerFunc {
 	}
 }
 
-func createItemHandler(formatter *render.Render) http.HandlerFunc {
+func createItemHandler(formatter *render.Render) http.HandlerFunc{
 	return func(response http.ResponseWriter, req *http.Request){
 
 		var create_item MenuItem
 		_ = json.NewDecoder(req.Body).Decode(&create_item)
 		session, error := mgo.Dial(database_server)
-		if error = session.DB(database_server).Login(mongo_user, mongo_pass);
+		if error = session.DB(database).Login(mongo_user, mongo_pass);
 		error != nil {
-			formatter.JSON(response, http.StatusInternalServerError, "Login Error")
+			formatter.JSON(response, http.StatusInternalServerError, "Error")
 			return
 		}
 		defer session.Close()
